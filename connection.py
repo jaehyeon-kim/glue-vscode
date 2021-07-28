e6c88b900bc5e5200a984bd31d372a33253bdcab
Jaehyeon Kim <dottami@gmail.com>
2021-07-28 22:17:44 +0000

    check connection done

---
+++ connection.py
@@ -0,0 +1,21 @@
+import sys
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.utils import getResolvedOptions
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+spark = glueContext.spark_session
+
+## read from S3
+ref_data = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    connection_options={
+        "paths": ["s3://analytics-workshop-20210721/data/reference_data/tracks_list.json"]
+    },
+    format="json",
+)
+
+## read from catalog
+ref_data1 = glueContext.create_dynamic_frame.from_catalog(
+    database="analyticsworkshopdb", table_name="reference_data", transformation_ctx="ref_data"
+)
